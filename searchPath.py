b8e06f29f925eeb87f56c345a8ac60d64250abfe
patelomi <91142232+patelomi@users.noreply.github.com>
2022-01-11 10:53:24 +0530

    Update searchPath.py
    changes on path

--- searchPath.py
+++ searchPath.py
@@ -1,17 +1,25 @@
 import json
 
 def recDict():
+    #give a path of file
     f = open("C:/Users/OMI PATEL/Desktop/demo/DS/test_data.json")
+    #load a json file in to the data var
     data = json.load(f)
-    
-    userInput = input("Enter entity Id :- ")
-
+    path = " "
+    #enter the id
+    userInput = input("Enter Entity Id :- ")#51
 
     for line in data:
+        #if user input is = to the dec
         if(line['entity_id'] == userInput):
+            #loop for the given id
             for secLine in data:
-                print(secLine['entity_id'])
-                if(secLine['entity_id'] == line['entity_id'] ):
-                    break
+                    #print path
+                    path = path+secLine['entity_id']+"/"#1/ then second time 1/2 and so on
+                    #check the condition if given id=51 then start for the begining until >=51 then break
+                    if(secLine['entity_id'] >= line['entity_id'] ):
+                        break
+    #print the path
+    print(path)
 
 recDict()
