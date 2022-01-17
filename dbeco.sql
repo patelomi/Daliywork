d4f59f353256ead7c507630d85a39754be951a43
patelomi <91142232+patelomi@users.noreply.github.com>
2022-01-17 23:01:00 +0530

    Add files via upload
    add data in the table

--- dbeco.sql
+++ dbeco.sql
@@ -9,30 +9,13 @@ create table customerInfo
     cust_password char(8) not null,
     cust_repassword char(8) not null,
     gender enum ('Male','Femal'),
-    par_address varchar(255) not null,
     primary key(customer_id)
 );
 
+drop table customerInfo;
+
 alter table customerInfo 
 add unique key (email);
-
-create table category
-(
-	category_id int auto_increment,
-    catagory_name char(100) not null,
-    catagory_description varchar(255),
-    primary key(category_id)
-);
-
-create table subCategory
-(
-	subcategory_id int auto_increment,
-    subcatagory_name char(100) not null,
-    subcatagory_description varchar(255),
-    category_id int,
-    primary key(subcategory_id),
-    foreign key (category_id) references category(category_id)
-);
 
 create table product
 (
@@ -44,10 +27,71 @@ create table product
     foreign key(category_id) references category(category_id) on delete cascade 
 );
 
-alter table product
-add column subcategory_id int;
+CREATE TABLE customerOrder (
+    order_id INT AUTO_INCREMENT,
+    customer_id INT,
+    address_id INT,
+    product_id INT,
+    PRIMARY KEY (order_id),
+    FOREIGN KEY (customer_id)
+        REFERENCES customerInfo (customer_id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (product_id)
+        REFERENCES product (product_id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (address_id)
+        REFERENCES address (address_id)
+        ON DELETE CASCADE
+);
+drop table customerOrder;
+select * from customerInfo;
 
-alter table product
-add foreign key(subcategory_id) references subcategory(subcategory_id) on delete cascade;
+CREATE TABLE address (
+    address_id INT AUTO_INCREMENT,
+    address varchar(100) not null,
+    customer_id INT,
+    PRIMARY KEY (address_id),
+    FOREIGN KEY (customer_id)
+        REFERENCES customerInfo (customer_id)
+        ON DELETE CASCADE
+);
+
+drop table address;
+
+CREATE TABLE category (
+    category_id INT AUTO_INCREMENT,
+    category_name CHAR(20) NOT NULL,
+    subcategory_id INT,
+    subcategory_name CHAR(20) NOT NULL,
+    PRIMARY KEY (category_id),
+    FOREIGN KEY (subcategory_id)
+        REFERENCES category (category_id)
+);
+select * from category;
+insert into customerInfo values(1,'omi','patel','omi@gmail.com','1234567','1234567','Femal');
+insert into customerInfo values(2,'Akshat','patel','akki@gmail.com','1234567','1234567','Male');
+insert into customerInfo values(3,'Sunita','patel','sunita@gmail.com','1234567','1234567','Femal');
+insert into customerInfo values(4,'Vedant','acharya','ved@gmail.com','1234567','1234567','Male');
+insert into customerInfo values(5,'Sangita','patel','sangita@gmail.com','1234567','1234567','Femal');
+insert into customerInfo values(6,'Tarak','patel','tarak@gmail.com','1234567','1234567','Male');
+insert into customerInfo values(7,'Manshi','jain','manshi@gmail.com','1234567','1234567','Femal');
+insert into customerInfo values(8,'Pawan','takkar','pawan@gmail.com','1234567','1234567','Male');
+SELECT 
+    *
+FROM
+    customerInfo;
+    
+insert into address values(1,'chandlodiya',1);
+insert into address values(2,'parbhatchok',2);
+insert into address values(3,'ankur',3);
+insert into address values(4,'scicity',4);
+insert into address values(5,'chandlodiya',5);
+insert into address values(6,'kalupur',6);
+insert into address values(7,'ranip',7);
+insert into address values(8,'kalupur',8);
+insert into address values(9,'shola bhavat',1);
+
+select * from address where customer_id=1;
 
 
+ 
